9a55a02e36f1cc71849b896627536907e4cd814f
hulya balci <hulyabalci07@gmail.com>
2020-10-13 19:42:07 +0200

    crash commit2

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("First Feature3 commit");
 
-        //actually this must crash with remote //we are simulating this change from my coworker
+     //i changed everyting start from beginning
         
     }
 }

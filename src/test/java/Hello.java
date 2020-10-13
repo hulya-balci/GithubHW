a1586d414050f5a6310fe2b0344469f008b60f43
hulya balci <hulyabalci07@gmail.com>
2020-10-13 19:32:33 +0200

    crashing commit

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("First Feature3 commit");
 
-        //actually
+        //actually this must crash with remote
     }
 }

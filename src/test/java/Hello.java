029f11fcba18b5a4c20b5e5d98a409bf75bf7c98
hulya-balci <72078001+hulya-balci@users.noreply.github.com>
2020-10-13 19:39:46 +0200

    Update Hello.java

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("First Feature3 commit");
 
-        //actually this must crash with remote
+        //actually this must crash with remote //we are simulating this change from my coworker
+        
     }
 }
